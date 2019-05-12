9c74d9ed96934a34eaa376caacb79bc070fc3f7b
alalh100 <alalh100@hhu.de>
2019-05-12 15:27:38 +0200

    neue Klasse "Rover" erstellt
    Die neue Klasse definiert neuen Datentyp Rover, der eine Position
    und eine Richtung hat.
    
    Die Klasse enthält auch die Methoden, die die Bewegungen des Rovers
    realisieren.

---
+++ src/main/java/rover/Rover.java
@@ -0,0 +1,48 @@
+package rover;
+
+public class Rover {
+
+    private int[] position;
+    private char richtung;
+
+
+    public Rover( int[] position, char richtung){
+
+        this.position = position;
+        this.richtung = richtung;
+    }
+
+    public void turnLeft(){
+
+        if      ( richtung =='^')  richtung = '<';
+        else if ( richtung == '<') richtung = 'v';
+        else if ( richtung == 'v') richtung = '>';
+        else if ( richtung == '>') richtung = '^';
+
+    }
+
+    public void turnRight(){
+
+        if      ( richtung == '^') richtung = '>';
+        else if ( richtung == '<') richtung = '^';
+        else if ( richtung == 'v') richtung = '<';
+        else if ( richtung == '>') richtung = 'v';
+
+    }
+
+    public void goForward(){
+
+        if      (richtung == '^' ) position[0]--;
+        else if (richtung == '<' ) position[1]--;
+        else if (richtung == 'v' ) position[0]++;
+        else if (richtung == '>' ) position[1]++;
+    }
+
+    public void goBackward(){
+
+        if      (richtung == '^' ) position[0]++;
+        else if (richtung == '<' ) position[1]++;
+        else if (richtung == 'v' ) position[0]--;
+        else if (richtung == '>' ) position[1]--;
+    }
+}
